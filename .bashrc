b9d3655cad28cc2397af7a6ae77daf21570f45b5
JianZcar <esteban.jianzcar@outlook.com>
2025-01-10 23:25:25 +0800

    now uses a bash script for the prompt

--- .bashrc
+++ .bashrc
@@ -12,54 +12,7 @@ if [ -f /etc/bashrc ]; then
     . /etc/bashrc
 fi
 
-function parse_git_branch {
-  if git rev-parse --is-inside-work-tree &>/dev/null; then
-    local branch
-    branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-    if [ -n "$branch" ]; then
-      local status=""
-      local ahead=$(git rev-list --count @{u}..HEAD 2>/dev/null)
-      local behind=$(git rev-list --count HEAD..@{u} 2>/dev/null)
-      if [ -n "$ahead" ] && [ "$ahead" -gt 0 ]; then
-        status+="$(printf '\ueb71')" 
-      fi
-      if [ -n "$behind" ] && [ "$behind" -gt 0 ]; then
-        status+="$(printf '\ueb6e')" 
-      fi
-      echo "[$branch$status]"
-    fi
-  fi
-}
-
-function abbreviate_path {
-  local full_path=$1
-  local abbreviated_path=""
-  local real_home=$(realpath $HOME)
-  # Check if the path is within the home directory
-  if [[ $full_path == "$real_home"* ]]; then
-    full_path="~/${full_path#$real_home}"
-  fi
-  
-  if [[ $full_path == "$HOME"* ]]; then
-    full_path="~/${full_path#$HOME}"
-  fi
-  
-  local IFS='/'
-  read -ra path_parts <<< "$full_path"
-  
-  # Abbreviate each part of the path
-  for part in "${path_parts[@]}"; do
-    if [[ $part == ${path_parts[-1]} || $part == "~" ]]; then
-      abbreviated_path+="$part"
-    else
-      abbreviated_path+="${part:0:1}/"
-    fi
-  done
-  
-  echo "$abbreviated_path"
-}
-
-PS1='\[\e[32m\][\[\e[34m\]\u@\h \[\e[35m\]bash \[\e[33m\]$(abbreviate_path "$PWD")\[\e[32m\]$(parse_git_branch)\[\e[32m\]]> \[\e[0m\]'
+PS1=$(source .prompt $PWD bash)
 
 # User specific environment
 if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,66 +1,8 @@
-function parse_git_branch
-  if git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set branch (git symbolic-ref --short HEAD)
-    if test -n "$branch"
-      set ahead (git rev-list --count @{u}..HEAD)
-      set behind (git rev-list --count HEAD..@{u})
-      if test -n "$ahead" -a "$ahead" -gt 0
-        set -a git_status (string join '' "$git_status" \ueb71)
-      end
-      if test -n "$behind" -a "$behind" -gt 0
-        set -a git_status (string join '' "$git_status" \ueb6e)
-      end
-      echo "[$branch$git_status]"
-    end
-  end
-end
-
-function abbreviate_path
-  set -l full_path $argv[1]
-  set -l abbreviated_path ""
-  set -l real_home (realpath $HOME)
-  
-  # Check if the path is within the home directory
-  if string match -q "$real_home*" "$full_path"
-    set full_path "~/"(string replace -r "^$real_home" "" $full_path)
-  end
-  
-  if string match -q "$HOME*" "$full_path"
-    set full_path "~/"(string replace -r "^$HOME" "" $full_path)
-  end
-  
-  # Split the path into parts
-  set -l path_parts (string split / $full_path)
-  
-  # Abbreviate each part of the path
-  for part in $path_parts
-    if test "$part" = $path_parts[-1] -o "$part" = "~"
-      set abbreviated_path "$abbreviated_path$part"
-    else
-      set abbreviated_path "$abbreviated_path"(echo $part | cut -c1)"/"
-    end
-  end
-  echo $abbreviated_path
-  function fish_title
-    echo $abbreviated_path
-	end
-end
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 function fish_prompt
-  set_color green
-  echo -n "["
-  set_color blue
-  echo -n (whoami)"@"(hostname)" "
-  set_color magenta
-  echo -n "fish " 
-  set_color yellow
-  echo -n (abbreviate_path $PWD)
-  set_color green
-  echo -n (parse_git_branch)
-  echo -n "]> "
-  set_color normal
+    # Capture the output of the Bash script
+    echo -e (~/.prompt $PWD 'fish')
 end
